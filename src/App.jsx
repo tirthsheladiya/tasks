085324412950b43499a11a103f93876d7b68ab6a
tirth sheladiya <tirthsheladiya989@gmail.com>
2025-07-22 18:40:03 +0530

    okk

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ const App = () => {
         "Productive agents are happy agents. Give them all the support tools and information they need to best serve your customers.",
     },
     {
-      img: "/illust-2.png",
+      img: "/Illust-2.png",
       title: "Best Integrations",
       description:
         "Our software is powerful enough to handle the most complex business, yet flexible enough to scale with you as you grow.",
     },
     {
-      img: "/illust-3.png",
+      img: "/Illust-3.png",
       title: "Grow Without Problems",
       description:
         "with evaluating in depth variety of data sets and including the speed of tech adaptation we can build bridges between any companies and their customers.",
